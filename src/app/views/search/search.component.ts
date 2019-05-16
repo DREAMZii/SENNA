7889a89cc6f35727a28b8705c79c351719bbb8bc
Tim Dollmann <tim.dollmann@kiongroup.com>
2019-05-16 16:58:45 +0200

    Make search even more pretty

--- src/app/views/search/search.component.ts
+++ src/app/views/search/search.component.ts
@@ -14,6 +14,12 @@ export class SearchComponent implements OnInit {
     d3.select('#first-search-button')
       .on('click', function() {
         const button = d3.select(this);
+        const buttonField = d3.select('#search-extend');
+
+        buttonField.style('height', '7.5vh');
+        buttonField.transition()
+          .duration(500)
+          .style('width', '37vh');
 
         button.transition()
           .duration(500)
